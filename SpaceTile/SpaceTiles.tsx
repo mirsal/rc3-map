1a472d32f7bd4f6609e30e49a771d99dd5bcf73e
mirsal <mirsal@mirsal.fr>
2020-12-06 15:00:05 +0000

    Testing shit

---
+++ SpaceTile/SpaceTiles.tsx
@@ -0,0 +1,163 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="SpaceTiles" tilewidth="128" tileheight="128" tilecount="53" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="128" source="Door type 1/DoorCenteDown.png"/>
+ </tile>
+ <tile id="1">
+  <image width="128" height="128" source="Door type 1/DoorCenterUp.png"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="128" source="Door type 1/DoorEmpty.png"/>
+ </tile>
+ <tile id="3">
+  <image width="128" height="128" source="Door type 1/DoorLeftCenter.png"/>
+ </tile>
+ <tile id="4">
+  <image width="128" height="128" source="Door type 1/DoorLeftDown.png"/>
+ </tile>
+ <tile id="5">
+  <image width="128" height="128" source="Door type 1/DoorLeftUp.png"/>
+ </tile>
+ <tile id="6">
+  <image width="128" height="128" source="Door type 1/DoorRightCenter.png"/>
+ </tile>
+ <tile id="7">
+  <image width="128" height="128" source="Door type 1/DoorRightDown.png"/>
+ </tile>
+ <tile id="8">
+  <image width="128" height="128" source="Door type 1/DoorRightUp.png"/>
+ </tile>
+ <tile id="9">
+  <image width="128" height="128" source="Ground type 1/GroundCenter.png"/>
+ </tile>
+ <tile id="10">
+  <image width="128" height="128" source="Ground type 1/GroundLeft.png"/>
+ </tile>
+ <tile id="11">
+  <image width="128" height="128" source="Ground type 1/GroundRight.png"/>
+ </tile>
+ <tile id="12">
+  <image width="128" height="128" source="Pipe type 1/PipeCenter.png"/>
+ </tile>
+ <tile id="13">
+  <image width="128" height="128" source="Pipe type 1/PipeCenterJoint.png"/>
+ </tile>
+ <tile id="21">
+  <image width="128" height="128" source="Tile props/Grate.png"/>
+ </tile>
+ <tile id="22">
+  <image width="128" height="128" source="Tile props/Screen1.png"/>
+ </tile>
+ <tile id="23">
+  <image width="128" height="128" source="Tile props/Screen2.png"/>
+ </tile>
+ <tile id="24">
+  <image width="128" height="128" source="Tile props/Screen3.png"/>
+ </tile>
+ <tile id="25">
+  <image width="128" height="128" source="Tile props/WallCenter5_Down.png"/>
+ </tile>
+ <tile id="26">
+  <image width="128" height="128" source="Tile props/WallCenter5.png"/>
+ </tile>
+ <tile id="27">
+  <image width="128" height="128" source="Tile props/WallCenter5Up.png"/>
+ </tile>
+ <tile id="28">
+  <image width="128" height="128" source="Tile props/WallTile3.png"/>
+ </tile>
+ <tile id="29">
+  <image width="128" height="128" source="Tile props/WallTileLanter 2_Down.png"/>
+ </tile>
+ <tile id="30">
+  <image width="128" height="128" source="Tile props/WallTileLanter 2_Up.png"/>
+ </tile>
+ <tile id="31">
+  <image width="128" height="128" source="Tile props/WallTileLanter 2.png"/>
+ </tile>
+ <tile id="32">
+  <image width="128" height="128" source="Tile props/WallTileLanter 3_Down.png"/>
+ </tile>
+ <tile id="33">
+  <image width="128" height="128" source="Tile props/WallTileLanter 3_Up.png"/>
+ </tile>
+ <tile id="34">
+  <image width="128" height="128" source="Tile props/WallTileLanter 3.png"/>
+ </tile>
+ <tile id="35">
+  <image width="128" height="128" source="Tile props/WallTileLanter 4_Down.png"/>
+ </tile>
+ <tile id="36">
+  <image width="128" height="128" source="Tile props/WallTileLanter 4_Up.png"/>
+ </tile>
+ <tile id="37">
+  <image width="128" height="128" source="Tile props/WallTileLanter 4.png"/>
+ </tile>
+ <tile id="38">
+  <image width="128" height="128" source="Wall type 1/Up/WallCenterUp1.png"/>
+ </tile>
+ <tile id="39">
+  <image width="128" height="128" source="Wall type 1/Up/WallCenterUp2.png"/>
+ </tile>
+ <tile id="40">
+  <image width="128" height="128" source="Wall type 1/RightEdge/WallEdgeRight.png"/>
+ </tile>
+ <tile id="41">
+  <image width="128" height="128" source="Wall type 1/RightEdge/WallEdgeRight1.png"/>
+ </tile>
+ <tile id="42">
+  <image width="128" height="128" source="Wall type 1/LeftEdge/WallEdgeLeft.png"/>
+ </tile>
+ <tile id="43">
+  <image width="128" height="128" source="Wall type 1/LeftEdge/WallEdgeLeft1.png"/>
+ </tile>
+ <tile id="44">
+  <image width="128" height="128" source="Wall type 1/Center/WallCenter.png"/>
+ </tile>
+ <tile id="45">
+  <image width="128" height="128" source="Wall type 1/Center/WallCenter3.png"/>
+ </tile>
+ <tile id="46">
+  <image width="128" height="128" source="Wall type 1/Center/WallLantern.png"/>
+ </tile>
+ <tile id="47">
+  <image width="128" height="128" source="Wall type 1/Bottom/WallCenterBottom1.png"/>
+ </tile>
+ <tile id="48">
+  <image width="128" height="128" source="Wall type 1/Bottom/WallCenterBottom2.png"/>
+ </tile>
+ <tile id="49">
+  <image width="128" height="128" source="PoleTile.png"/>
+ </tile>
+ <tile id="50">
+  <image width="128" height="128" source="PoleTile1.png"/>
+ </tile>
+ <tile id="51">
+  <image width="128" height="128" source="Window/WindowBottomCenter.png"/>
+ </tile>
+ <tile id="52">
+  <image width="128" height="128" source="Window/WindowBottomEdge.png"/>
+ </tile>
+ <tile id="53">
+  <image width="128" height="128" source="Window/WindowBottomEdgeRight.png"/>
+ </tile>
+ <tile id="54">
+  <image width="128" height="128" source="Window/WindowLeft.png"/>
+ </tile>
+ <tile id="55">
+  <image width="128" height="128" source="Window/WindowRight.png"/>
+ </tile>
+ <tile id="56">
+  <image width="128" height="128" source="Window/WindowsEmpty.png"/>
+ </tile>
+ <tile id="57">
+  <image width="128" height="128" source="Window/WindowUpCenter.png"/>
+ </tile>
+ <tile id="58">
+  <image width="128" height="128" source="Window/WindowUpEdgeLeft.png"/>
+ </tile>
+ <tile id="59">
+  <image width="128" height="128" source="Window/WindowUpEdgeRight.png"/>
+ </tile>
+</tileset>
---
+++ fantasy.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="fantasy" tilewidth="32" tileheight="32" tilecount="900" columns="30">
+ <image source="hyptosis_til-art-batch-2-1.png" width="960" height="960"/>
+</tileset>
